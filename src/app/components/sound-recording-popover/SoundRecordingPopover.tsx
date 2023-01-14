15e0a43017e521290e968872c8026628bc91b5d6
angusho1 <57027339+angusho1@users.noreply.github.com>
2023-01-13 23:12:22 -0800

    Customize Google Maps info window styles and close button

--- src/app/components/sound-recording-popover/SoundRecordingPopover.tsx
+++ src/app/components/sound-recording-popover/SoundRecordingPopover.tsx
@@ -1,13 +1,13 @@
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 import SoundRecording from 'models/SoundRecording.model';
-import { ActionIcon, Center, Container, Flex, Image, Loader, Stack, Text, Title } from '@mantine/core';
+import { ActionIcon, Center, Container, Flex, Group, Image, Loader, Stack, Text, Title } from '@mantine/core';
 import dayjs from 'dayjs';
 import localizedFormat from 'dayjs/plugin/localizedFormat';
 import { useAppDispatch, useAppSelector } from 'app/hooks';
-import { selectIsDetailedViewOpen, toggleDetailedView } from 'features/sound-clips/soundClipSlice';
+import { selectIsDetailedViewOpen, setSelectedSoundRecording, toggleDetailedView } from 'features/sound-clips/soundClipSlice';
 import ImageCarouselModal, { ImageModalState } from '../image-carousel/ImageCarouselModal';
 import './SoundRecordingPopover.css';
-import { IconArrowUpRightCircle } from '@tabler/icons';
+import { IconArrowUpRightCircle, IconX } from '@tabler/icons';
 import { useSoundRecordingFile, useSoundRecordingImageFiles } from 'app/hooks/sound-recording.hooks';
 import TagList from '../tag-list/TagList';
 
@@ -35,67 +35,74 @@ export default function SoundRecordingPopover(props: SoundRecordingPopoverProps)
     const isLoading = () => !recordingFile || (imageFiles.length !== soundRecording.imageFiles?.length);
 
     return (
-        <Container size={300} px="xs">
-            <ActionIcon onClick={() => dispatch(toggleDetailedView(!isDetailedViewOpen))}>
-                <IconArrowUpRightCircle size={18} />
-            </ActionIcon>
-            <Stack align="center" spacing={8}>
-                <Stack spacing={2} align="center">
-                    <Title order={4}>{soundRecording.title}</Title>
-                    <Text size="sm" fw={300}>Recorded by {soundRecording.author}</Text>
-                    <Text size="sm" fw={300}>Date: {dateStr}</Text>
-                    <Text component="p" fz="sm" fw={300}>
-                        {soundRecording.description}
-                    </Text>
-                </Stack>
-                <Stack align="center">
-                    {imageFiles && (
-                        <Flex gap={5}>
-                            {
-                                imageFiles.map((image, index) => (
-                                    <Image
-                                        className="popover-img"
-                                        sx={{ cursor: 'pointer' }}
-                                        key={image.objectUrl}
-                                        width={100}
-                                        height={80}
-                                        src={image.objectUrl}
-                                        onClick={() => {
-                                            setImageModalState({
-                                                opened: true,
-                                                selectedIndex: index,
-                                            });
-                                        }}
-                                    />
-                                ))
-                            }
-                        </Flex>
+        <Fragment>
+            <Group position="right" spacing={2}>
+                <ActionIcon onClick={() => dispatch(toggleDetailedView(!isDetailedViewOpen))}>
+                    <IconArrowUpRightCircle size={18} />
+                </ActionIcon>
+                <ActionIcon onClick={() => dispatch(setSelectedSoundRecording(null))}>
+                    <IconX size={18} />
+                </ActionIcon>
+            </Group>
+            <Container size={300} px="xs" sx={{ border: '3px' }}>
+                <Stack align="center" spacing={8}>
+                    <Stack spacing={2} align="center">
+                        <Title order={4}>{soundRecording.title}</Title>
+                        <Text size="sm" fw={300}>Recorded by {soundRecording.author}</Text>
+                        <Text size="sm" fw={300}>Date: {dateStr}</Text>
+                        <Text component="p" fz="sm" fw={300}>
+                            {soundRecording.description}
+                        </Text>
+                    </Stack>
+                    <Stack align="center">
+                        {imageFiles && (
+                            <Flex gap={5}>
+                                {
+                                    imageFiles.map((image, index) => (
+                                        <Image
+                                            className="popover-img"
+                                            sx={{ cursor: 'pointer' }}
+                                            key={image.objectUrl}
+                                            width={100}
+                                            height={80}
+                                            src={image.objectUrl}
+                                            onClick={() => {
+                                                setImageModalState({
+                                                    opened: true,
+                                                    selectedIndex: index,
+                                                });
+                                            }}
+                                        />
+                                    ))
+                                }
+                            </Flex>
+                        )}
+                        {recordingFile && (
+                            <audio controls src={recordingFile.objectUrl}></audio>
+                        )}
+                    </Stack>
+                    {isLoading() && (
+                        <Center>
+                            <Loader color={'pink'} />
+                        </Center>
                     )}
-                    {recordingFile && (
-                        <audio controls src={recordingFile.objectUrl}></audio>
-                    )}
+                    <Stack spacing={2} align="stretch" w="100%">
+                        { soundRecording.tags && soundRecording.tags.length > 0 && (
+                            <TagList tags={soundRecording.tags} />
+                        )}
+                    </Stack>
                 </Stack>
-                {isLoading() && (
-                    <Center>
-                        <Loader color={'pink'} />
-                    </Center>
-                )}
-                <Stack spacing={2} align="stretch" w="100%">
-                    { soundRecording.tags && soundRecording.tags.length > 0 && (
-                        <TagList tags={soundRecording.tags} />
-                    )}
-                </Stack>
-            </Stack>
 
-            <ImageCarouselModal
-                opened={imageModalState.opened}
-                selectedIndex={imageModalState.selectedIndex}
-                images={imageFiles}
-                onClose={() => setImageModalState({
-                    ...imageModalState,
-                    opened: false,
-                })}
-            />
-        </Container>
+                <ImageCarouselModal
+                    opened={imageModalState.opened}
+                    selectedIndex={imageModalState.selectedIndex}
+                    images={imageFiles}
+                    onClose={() => setImageModalState({
+                        ...imageModalState,
+                        opened: false,
+                    })}
+                />
+            </Container>
+        </Fragment>
     )
 }
