fa499577cc4e345f7d4ad60ba1e16a52887b89f2
angusho1 <57027339+angusho1@users.noreply.github.com>
2023-01-17 15:03:36 -0800

    Fix bug with app crash when navigating back to home page with selected recording

--- src/app/pages/home/Home.page.tsx
+++ src/app/pages/home/Home.page.tsx
@@ -1,7 +1,7 @@
 import { Drawer } from '@mantine/core';
 import SoundRecordingDetailedView from 'app/components/sound-recording-detailed-view/SoundRecordingDetailedView';
 import { useAppDispatch, useAppSelector } from 'app/hooks';
-import { selectCurrentSoundRecording, selectIsDetailedViewOpen, toggleDetailedView } from 'features/sound-clips/soundClipSlice';
+import { selectCurrentSoundRecording, selectIsDetailedViewOpen, setSelectedSoundRecording, toggleDetailedView } from 'features/sound-clips/soundClipSlice';
 import { Fragment, useEffect } from 'react';
 import SoundRecordingMap from '../../components/sound-recording-map/SoundRecordingMap';
 
@@ -13,6 +13,12 @@ export default function HomePage() {
   useEffect(() => {
     document.title = 'Chinatown Sound Map';
   });
+
+  useEffect(() => {
+    return () => {
+      dispatch(setSelectedSoundRecording(null));
+    }
+  }, [dispatch]);
 
   return (
     <Fragment>
